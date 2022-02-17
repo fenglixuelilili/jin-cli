b58097d66d463d8be741ae2aac439494aa8b5f2e
fenglixuelilili <1913849404@qq.com>
2022-02-17 20:50:19 +0800

    增加模板项目

--- config.js
+++ config.js
@@ -1,14 +1,20 @@
 module.exports = {
-  "complete": { // 完整项目模版
-    'scss': {
-      url: 'https://github.com/fenglixuelilili/Vue3.0-TS-Element.git', // 标准化scss模版
-      name: '标准化scss模版'
-    }
+  complete: {
+    // 完整项目模版
+    scss: {
+      url: "jyjgit@139.9.143.12:component/web-saas-template.git", // 标准化scss模版
+      name: "标准化scss模版",
+    },
   },
-  "modular": {
-    'ea': {
-      url: 'https://github.com/fenglixuelilili/uniapp-xbook.git', // 审批模块
-      name: '审批模块'
-    }
-  }
+  // 模块相关
+  modular: {
+    ea: {
+      url: "https://github.com/fenglixuelilili/uniapp-xbook.git", // 审批模块
+      name: "审批模块",
+    },
+    template: {
+      url: "jyjgit@139.9.143.12:component/web-saas-template-module.git", // 审批模块
+      name: "模块模板",
+    },
+  },
 }
--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const path =require('path')
-const fs = require('fs')
-// console.log(process.argv)
-console.log(path.join(__dirname, './dit'))
-console.log(path.join(__dirname, 'dit'))
-console.log(path.resolve(__dirname, './dit'))
-console.log(path.resolve(__dirname, 'dit'))
-
-fs.stat(path.join(__dirname, 'node_modules/.bin'), (err) => {
+const path = require("path")
+const fs = require("fs")
+fs.stat(path.join(__dirname, "node_modules/.bin"), (err) => {
   console.log(err)
   process.exit()
 })
